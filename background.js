0863533cd9c8d8b33b3ba8c225f0f393126b5668
nxrobins <nxrobinson@ucdavis.edu>
2023-03-21 00:31:18 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,27 +1,36 @@
+async function analyzeProfileData(profileData) {
+  const apiKey = "your_openai_api_key";
+  const apiEndpoint = "https://api.openai.com/v1/engines/davinci-codex/completions";
+  const prompt = `Analyze the LinkedIn profile of ${profileData.name}, who has the headline "${profileData.headline}".`;
+
+  const response = await fetch(apiEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 150,
+      n: 1,
+      stop: null,
+      temperature: 0.7,
+    }),
+  });
+
+  const data = await response.json();
+  return data.choices[0].text.trim();
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "analyzeProfile") {
-    const prompt = request.prompt;
-    const url = "https://api.openai.com/v1/engines/davinci-codex/completions";
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "fetchProfileData" }, async (profileData) => {
+        const analysis = await analyzeProfileData(profileData);
+        sendResponse({ analysis });
+      });
+    });
 
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer 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
-"
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: 150,
-        n: 1,
-        stop: null,
-        temperature: 0.8
-      })
-    })
-      .then(response => response.json())
-      .then(data => sendResponse(data.choices[0].text))
-      .catch(error => console.error("Error:", error));
-
-    return true; // Required for async sendResponse
+    return true;
   }
 });
