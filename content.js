28b530197e958ce57938c4da0a20193d80da1621
nxrobins <nxrobinson@ucdavis.edu>
2023-03-21 00:59:01 -0700

    Update content.js

--- content.js
+++ content.js
@@ -19,5 +19,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       console.error("Error fetching profile data:", error);
       sendResponse({ error: "Error fetching profile data." });
     }
+
+    // Add this line to keep the message channel open for asynchronous responses.
+    return true;
+  }
+});
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "fetchProfileData") {
+    try {
+      const name = document.querySelector(".top-card-layout__title").innerText;
+      const headline = document.querySelector(".top-card-layout__headline").innerText;
+
+      console.log(`Profile data: Name: ${name}, Headline: ${headline}`);
+      sendResponse({ name, headline });
+    } catch (error) {
+      console.error("Error fetching profile data:", error);
+      sendResponse({ error: "Error fetching profile data." });
+    }
   }
 });
