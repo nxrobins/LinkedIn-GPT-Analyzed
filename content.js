1932a5cc53f8010d14941fd8c0e60bbd58b459b5
nxrobins <nxrobinson@ucdavis.edu>
2023-03-21 00:30:46 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "fetchProfileData") {
+    const name = document.querySelector(".top-card-layout__title").innerText;
+    const headline = document.querySelector(".top-card-layout__headline").innerText;
 
+    sendResponse({ name, headline });
+  }
+});
